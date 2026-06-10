30927a5b03452e643660598f881efd4c8e2b73ad
Juan Pablo Meza Olivera <jmezao@unicartagena.edu.co>
2026-06-10 13:18:44 -0500

    agregar funcionalidad regitrar libro

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,2 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Biblioteca {
+
+    private static ArrayList<Libro> libros = new ArrayList<>();
+
+    public static void registrarLibro() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese el nombre del libro:");
+        String nombre = sc.nextLine();
+
+        System.out.println("Ingrese el autor del libro:");
+        String autor = sc.nextLine();
+
+        Libro libro = new Libro(nombre, autor);
+        libros.add(libro);
+
+        System.out.println("Libro registrado correctamente ✔");
+    }
+
+    public static void listarLibros() {
+
+        if (libros.isEmpty()) {
+            System.out.println("No hay libros registrados.");
+            return;
+        }
+
+        for (Libro l : libros) {
+            System.out.println(l);
+        }
+    }
+
+
+
 }
--- src/BibliotecaCli.java
+++ src/BibliotecaCli.java
@@ -5,49 +5,53 @@ public class BibliotecaCli {
     public static void iniciar() {
 
         Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("""
+                    === Biblioteca Personal ===
+                    1. Registrar libro
+                    2. Buscar libro
+                    3. Contar libros
+                    4. Listar libros
+                    5. Salir
+                    
+                    Seleccione una opción: """);
 
-        System.out.print("""
-                === Biblioteca Personal ===
-                1. Registrar libro
-                2. Buscar libro
-                3. Contar libros
-                4. Listar libros
-                5. Salir
+            String entrada = scanner.nextLine();
 
-                Seleccione una opción: """);
+            while (!entrada.equals("1")
+                    && !entrada.equals("2")
+                    && !entrada.equals("3")
+                    && !entrada.equals("4")
+                    && !entrada.equals("5")) {
 
-        String entrada = scanner.nextLine();
+                System.out.print("Ingrese una opción válida: ");
+                entrada = scanner.nextLine();
+            }
 
-        while (!entrada.equals("1")
-                && !entrada.equals("2")
-                && !entrada.equals("3")
-                && !entrada.equals("4")
-                && !entrada.equals("5")) {
+            int opcion = Integer.parseInt(entrada);
 
-            System.out.print("Ingrese una opción válida: ");
-            entrada = scanner.nextLine();
+            switch (opcion) {
+                case 1:
+                    System.out.println("Registrar libro");
+
+                    Biblioteca.registrarLibro();
+
+                    break;
+                case 2:
+                    System.out.println("Buscar libro");
+                    break;
+                case 3:
+                    System.out.println("Contar libros");
+                    break;
+                case 4:
+                    System.out.println("Listar libros");
+                    break;
+                case 5:
+                    System.out.println("Saliendo...");
+                    scanner.close();
+                    return;
+            }
+
+
         }
-
-        int opcion = Integer.parseInt(entrada);
-
-        switch (opcion) {
-            case 1:
-                System.out.println("Registrar libro");
-                break;
-            case 2:
-                System.out.println("Buscar libro");
-                break;
-            case 3:
-                System.out.println("Contar libros");
-                break;
-            case 4:
-                System.out.println("Listar libros");
-                break;
-            case 5:
-                System.out.println("Saliendo...");
-                break;
-        }
-
-        scanner.close();
-    }
-}
+    }}
