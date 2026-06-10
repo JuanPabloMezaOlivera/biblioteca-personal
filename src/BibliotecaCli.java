8c3522aae152a714de23d7d7d91db7db5e22b03b
Juan Pablo Meza Olivera <jmezao@unicartagena.edu.co>
2026-06-10 00:33:01 -0500

    crear menu de consola  prueba 1

--- src/BibliotecaCli.java
+++ src/BibliotecaCli.java
@@ -1,2 +1,53 @@
+import java.util.Scanner;
+
 public class BibliotecaCli {
+
+    public static void iniciar() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("""
+                === Biblioteca Personal ===
+                1. Registrar libro
+                2. Buscar libro
+                3. Contar libros
+                4. Listar libros
+                5. Salir
+
+                Seleccione una opción: """);
+
+        String entrada = scanner.nextLine();
+
+        while (!entrada.equals("1")
+                && !entrada.equals("2")
+                && !entrada.equals("3")
+                && !entrada.equals("4")
+                && !entrada.equals("5")) {
+
+            System.out.print("Ingrese una opción válida: ");
+            entrada = scanner.nextLine();
+        }
+
+        int opcion = Integer.parseInt(entrada);
+
+        switch (opcion) {
+            case 1:
+                System.out.println("Registrar libro");
+                break;
+            case 2:
+                System.out.println("Buscar libro");
+                break;
+            case 3:
+                System.out.println("Contar libros");
+                break;
+            case 4:
+                System.out.println("Listar libros");
+                break;
+            case 5:
+                System.out.println("Saliendo...");
+                break;
+        }
+
+        scanner.close();
+    }
 }
